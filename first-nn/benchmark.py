193bdaa5d77210a828e230e0b1af30bec8f41997
jens <jens@doellmann.com>
2021-08-05 20:48:36 +0200

    vscode update

--- first-nn/benchmark.py
+++ first-nn/benchmark.py
@@ -11,41 +11,42 @@ EDGETUP_LIB = {
 
 delegates = [tflite.load_delegate(EDGETUP_LIB)]
 
-sine_model_cpu = tflite.Interpreter("sine_model_quant.tflite")
+sine_model_cpu = tflite.Interpreter("sine_model.tflite")
+sine_model_quant_cpu = tflite.Interpreter("sine_model_quant.tflite")
 sine_model_tpu = tflite.Interpreter(
     "sine_model_quant_edgetpu.tflite", experimental_delegates=delegates
 )
 
 sine_model_cpu.allocate_tensors()
+sine_model_quant_cpu.allocate_tensors()
 sine_model_tpu.allocate_tensors()
 
-input_details_cpu = sine_model_cpu.get_input_details()[0]
-input_details_tpu = sine_model_tpu.get_input_details()[0]
-
-input_index_cpu = input_details_cpu["index"]
-input_index_tpu = input_details_tpu["index"]
-
-input_shape = input_details_cpu["shape"]
-
 rng = np.random.default_rng(42)
-test_data = rng.integers(low=0, high=6, size=[200000, *input_shape], dtype=np.int8)
 
-result = 0
-for i, value in enumerate(test_data):
-    sine_model_cpu.set_tensor(input_index_cpu, value)
-    start = time.perf_counter()
-    sine_model_cpu.invoke()
-    inference_time = (time.perf_counter() - start) * 1000
-    if i > 10:
-        result += inference_time
+
+def measure_performance(model: tflite.Interpreter):
+    input_details = model.get_input_details()[0]
+    input_index = input_details["index"]
+    input_shape = input_details["shape"]
+    dtype = input_details["dtype"]
+
+    test_data = rng.integers(low=0, high=6, size=[20000, *input_shape])
+    test_data = test_data.astype(dtype)
+
+    result = 0
+    for i, value in enumerate(test_data):
+        model.set_tensor(input_index, value)
+        start = time.perf_counter()
+        model.invoke()
+        inference_time = (time.perf_counter() - start) * 1000
+        if i > 10:
+            result += inference_time
+    return result
+
+
+result = measure_performance(sine_model_cpu)
 print(f"CPU Inferenz Zeit: {result} ms")
-
-result = 0
-for i, value in enumerate(test_data):
-    sine_model_cpu.set_tensor(input_index_tpu, value)
-    start = time.perf_counter()
-    sine_model_cpu.invoke()
-    inference_time = (time.perf_counter() - start) * 1000
-    if i > 10:
-        result += inference_time
+result = measure_performance(sine_model_quant_cpu)
+print(f"CPU quantisierte Inferenz Zeit: {result} ms")
+result = measure_performance(sine_model_tpu)
 print(f"TPU Inferenz Zeit: {result} ms")
