b939e908046f8a6c1bc163b945301074f1a9c75a
jens <jens@doellmann.com>
2021-08-05 21:03:31 +0200

    update compare

--- first-nn/compare.py
+++ first-nn/compare.py
@@ -12,7 +12,6 @@ EDGETUP_LIB = {
 delegates = [tflite.load_delegate(EDGETUP_LIB)]
 
 insect_model_cpu = tflite.Interpreter("mobilenet_v2_inat_insect_quant.tflite")
-
 insect_model_tpu = tflite.Interpreter(
     "mobilenet_v2_inat_insect_quant_edgetpu.tflite", experimental_delegates=delegates
 )
@@ -20,37 +19,29 @@ insect_model_tpu = tflite.Interpreter(
 insect_model_cpu.allocate_tensors()
 insect_model_tpu.allocate_tensors()
 
-input_details_cpu = insect_model_cpu.get_input_details()[0]
-input_details_tpu = insect_model_tpu.get_input_details()[0]
-
-input_index_cpu = input_details_cpu["index"]
-input_index_tpu = input_details_tpu["index"]
-
-input_shape = input_details_cpu["shape"][1:]
 
 rng = np.random.default_rng(42)
-test_data = rng.integers(low=0, high=256, size=input_shape, dtype=np.uint8)
-test_data = np.expand_dims(test_data, axis=0)
 
-result = 0
 
-for i in range(10):
-    insect_model_cpu.set_tensor(input_index_cpu, test_data)
-    start = time.perf_counter()
-    insect_model_cpu.invoke()
-    inference_time = (time.perf_counter() - start) * 1000
-    result += inference_time
+def measure_performance(model: tflite.Interpreter):
+    input_details = model.get_input_details()[0]
+    input_index = input_details["index"]
+    input_shape = input_details["shape"][1:]
 
+    test_data = rng.integers(low=0, high=256, size=[1, *input_shape], dtype=np.uint8)
+
+    result = 0
+    for i in range(11):
+        model.set_tensor(input_index, test_data)
+        start = time.perf_counter()
+        model.invoke()
+        inference_time = (time.perf_counter() - start) * 1000
+        if i > 0:
+            result += inference_time
+    return result
+
+
+result = measure_performance(insect_model_cpu)
 print(f"CPU Inferenz Zeit: {result} ms")
-
-result = 0
-
-for i in range(11):
-    insect_model_tpu.set_tensor(input_index_tpu, test_data)
-    start = time.perf_counter()
-    insect_model_tpu.invoke()
-    inference_time = (time.perf_counter() - start) * 1000
-    if i > 0:
-        result += inference_time
-
+result = measure_performance(insect_model_tpu)
 print(f"TPU Inferenz Zeit: {result} ms")
