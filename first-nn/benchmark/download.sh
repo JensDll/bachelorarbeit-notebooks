72ff9649958cba87bbb2d9ce12c8db3a05168057
jens <jens@doellmann.com>
2021-08-06 15:33:12 +0200

    move benchmark to folder

---
+++ first-nn/benchmark/download.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+[ -e "models" ] && rm -ri models
+
+mkdir models && \
+wget -q -O - "https://github.com/JensDll/bachelorarbeit-notebooks/releases/download/$1/sine_models.tar.gz" | \
+    tar --extract --gzip --directory=models -f -
+wget -O models/mobilenet_benchmark.py "https://raw.githubusercontent.com/JensDll/bachelorarbeit-notebooks/$1/first-nn/benchmark/mobilenet_benchmark.py" && \
+wget -O models/sine_benchmark.py "https://raw.githubusercontent.com/JensDll/bachelorarbeit-notebooks/$1/first-nn/benchmark/sine_benchmark.py" && \
+wget -O models/mobilenet_v2_1.0_224_inat_insect_quant_edgetpu.tflite \
+    "https://github.com/google-coral/test_data/raw/master/mobilenet_v2_1.0_224_inat_insect_quant_edgetpu.tflite" && \
+wget -O models/mobilenet_v2_1.0_224_inat_insect_quant.tflite \
+    "https://github.com/google-coral/test_data/raw/master/mobilenet_v2_1.0_224_inat_insect_quant.tflite"
